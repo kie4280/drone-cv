c758a412b65e85ca89190ab552ed3088f3917ef2
kie4280 <kie4280ann@gmail.com>
2021-03-18 20:37:43 +0800

    step1

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,23 +1,55 @@
+from typing import Counter
 import cv2
 import numpy as np
+from numpy.core.defchararray import index
+
+# connect -2 means conflict
 
 def getConnected(frame):
     mask = getFore (frame)
-    connect= np.zeros(frame.shape,dtype=np.int)
-    cat_counter:int = 1
+    connect= np.zeros((frame.shape[0], frame.shape[1]),dtype=np.int)
+    cat_counter:int = 0
     area_counter:dict = dict()
+    index_mapping:dict = dict()
     
     it = np.nditer(mask, flags=['multi_index'], op_flags=['readonly'])
     for x in it:
-        
-        cur = it.multi_index()
-        if cur[0] == 0:
-            if cur[1] == 0:
-                connect[cur] = 1
+        cur_pos = it.multi_index
+        left = 0 if cur_pos[1] == 0 else connect[cur_pos[0], cur_pos[1]-1]
+        top = 0 if cur_pos[0] == 0 else connect[cur_pos[0]-1, cur_pos[1]]
+
+        if x == 0:
+            continue
+        if left == 0 and top == 0:
+            cat_counter += 1
+            connect[cur_pos] = cat_counter
+            index_mapping[cat_counter] = cat_counter
+        elif left != 0 and top != 0:
+            if left != top:
+                connect[cur_pos] = -2
             else:
-                if connect[cur[1]-1] == cat_counter:
-                    connect[cur] = cat_counter
+                connect[cur_pos] = left
+        else:
+            if left != 0:
+                connect[cur_pos] = left
+            else:
+                connect[cur_pos] = top
         print(it.multi_index)
+
+    it = np.nditer(connect, flags=['multi_index'], op_flags=['readonly'])
+    for x in it:
+        cur_pos = it.multi_index
+        left = 0 if cur_pos[1] == 0 else connect[cur_pos[0], cur_pos[1]-1]
+        top = 0 if cur_pos[0] == 0 else connect[cur_pos[0]-1, cur_pos[1]]
+        if x == -2:
+            m1 = 0;m2 = 0
+            if left < top:
+                m1 = left
+                m2 = top
+            else :
+                m1 = top
+                m2 = left
+            index_mapping[m2] = m1
 
 def getFore(frame):
     backSub = cv2.createBackgroundSubtractorMOG2()
