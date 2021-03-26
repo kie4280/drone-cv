36f2c42717a8bd24837d01507fe364ab001db6bf
Po-Jen-Chen <80053500+Po-Jen-Chen@users.noreply.github.com>
2021-03-27 00:55:21 +0800

    Update lab4_2.py

--- lab4/lab4_2.py
+++ lab4/lab4_2.py
@@ -1,78 +1,68 @@
 import cv2 
 import numpy as np
 
-p1 = (202, 246)
-p2 = (246, 462)
-p3 = (631, 260)
-p4 = (767, 433)
+p1 = (202, 242)
+p2 = (241, 464)
+p3 = (631, 255)
+p4 = (765, 432)
 
-       
-img = cv2.imread('warp.jpg',cv2.IMREAD_COLOR)  
-
-img_height, img_width= np.shape(img)[0], np.shape(img)[1]
-src = np.float32([[0,0], [0,img_width-1], [img_height-1,0], [img_height-1, img_width-1]])
-dst = np.float32([p1, p2, p3, p4])
-
-transform = cv2.getPerspectiveTransform(src, dst)
-
-
-def bilinear(img):
-    buf = np.zeros((3*img.shape[0], 3*img.shape[1],
-                    img.shape[2]), dtype=np.uint8)
-    for i in range(3*img.shape[0]):
-        for j in range(3*img.shape[1]):
-            SrcX=float((i+0.5)/3-0.5)
-            SrcY=float((j+0.5)/3-0.5)
-            if int(SrcX) >= img.shape[0]-2 and int(SrcY) >= img.shape[1]-2:
-                buf[i,j,:]=img[int(SrcX), int(SrcY), :]            
-            elif int(SrcX) >= img.shape[0]-2:
-                f_1 = img[int(SrcX), int(SrcY), :] 
-                f_2 = img[int(SrcX),int(SrcY)+1, :]
-                buf[i, j, :] = (int(SrcY)+1-SrcY)*f_1+(SrcY-int(SrcY))*f_2 
-            elif int(SrcY) >= img.shape[1]-2:
-                buf[i, j, :] = (int(SrcX)+1-SrcX)*img[int(SrcX), int(SrcY), :] + \
-                (SrcX-int(SrcX))*img[int(SrcX)+1, int(SrcY), :]
-            else:
-                f_1 = (int(SrcX)+1-SrcX)*img[int(SrcX), int(SrcY), :] + \
-                    (SrcX-int(SrcX))*img[int(SrcX)+1, int(SrcY), :]
-                f_2 = (int(SrcX)+1-SrcX)*img[int(SrcX),int(SrcY)+1, :] + \
-                    (SrcX-int(SrcX))*img[int(SrcX)+1,int(SrcY)+1 , :]
-                buf[i, j, :] = (int(SrcY)+1-SrcY)*f_1+(SrcY-int(SrcY))*f_2
-    return buf
-
-
-def project(image, video):
-    
-    buf = np.zeros(image.shape)
-    invers=np.linalg.inv(transform)
-    it = np.nditer(connect, flags=['multi_index'], op_flags=['readwrite'])
-    for x in it:
-
-        tran = np.multiply(transform, multi_index)
-        if tran
-
-    print(transform)
+def project(image, video,transform):
+    video_shape=(np.shape(video)[0],np.shape(video)[1])
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            cur_pos=(i,j,1)
+            response=np.matmul(transform,cur_pos)
+            response_x=response[0]/response[2]
+            response_y=response[1]/response[2]
+            #print(np.matmul(invers,(243,462,1)))
+            if (response_x>=0 and response_x<= video.shape[0]) and (response_y>=0 and response_y<= video.shape[1]):
+                if int(response_x) >= video_shape[0]-1 and int(response_y) >= video_shape[1]-1:
+                    image[i,j,:]=video[int(response_x), int(response_y), :]            
+                elif int(response_x) >= video_shape[0]-1:
+                    f_1 = video[int(response_x), int(response_y), :] 
+                    f_2 = video[int(response_x),int(response_y)+1, :]
+                    image[i,j,:]= (int(response_y)+1-response_y)*f_1+(response_y-int(response_y))*f_2 
+                elif int(response_y) >= video_shape[1]-1:
+                    image[i,j,:]= (int(response_x)+1-response_x)*video[int(response_x), int(response_y), :] + \
+                    (response_x-int(response_x))*video[int(response_x)+1, int(response_y), :]
+                else:
+                    f_1 = (int(response_x)+1-response_x)*video[int(response_x), int(response_y), :] + \
+                        (response_x-int(response_x))*video[int(response_x)+1, int(response_y), :]
+                    f_2 = (int(response_x)+1-response_x)*video[int(response_x),int(response_y)+1, :] + \
+                        (response_x-int(response_x))*video[int(response_x)+1,int(response_y)+1 , :]
+                    image[i,j,:] = (int(response_y)+1-response_y)*f_1+(response_y-int(response_y))*f_2
+    return image
 
 
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(1)
-    img = cv2.imread('warp.jpg',cv2.IMREAD_COLOR)  
+    img = cv2.imread('warp.jpg',cv2.IMREAD_COLOR)
+    img_height, img_width= np.shape(img)[0], np.shape(img)[1]
+    dst = np.float32([p1, p2, p3, p4])  
     while cap.isOpened():
         ret, frame = cap.read()
-
         if ret == False:
             break
         cv2.imshow("actual", frame)
+        src_height,src_width = np.shape(frame)[0] , np.shape(frame)[1]
+        src = np.float32([(0,0),  (0,src_width-1), (src_height-1,0), (src_height-1, src_width-1)])
+        transform = cv2.getPerspectiveTransform(dst, src)
         key = cv2.waitKey(1)
         if key  == ord("q"):
-
             print("capture")
+            result = project(img,frame,transform)
         elif key == ord("b"):
             break
-        tranform = project(img, frame)
-
-    cv2.imshow('test',img)
-
+        #print(np.matmul(transform,(0,src_width-1,0)),'  ' ,np.matmul(transform,(src_height-1, src_width-1,1)))
+        #print(transform)
+    cv2.namedWindow("org",0)
+    cv2.imshow("org",result)
+    # print(src)
+    # print(dst)
+    # transform = cv2.getPerspectiveTransform(dst, src)
+    # print(transform)
+    # print(np.matmul(transform,(241, 464,1))/np.matmul(transform,(241, 464,1))[2],'  ' ,np.matmul(transform,(765, 432,1))/np.matmul(transform,(761, 432,1))[2])
+    cv2.imwrite('/Users/Jacky Chen/Pictures/qqq.jpg',result)
     cv2.waitKey(0)  
     cv2.destroyAllWindows()
