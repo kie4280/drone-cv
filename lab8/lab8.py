648c477c06f0adb4f6b28c7328f57288d8817152
kie4280 <kie4280ann@gmail.com>
2021-05-03 15:35:06 +0800

    can detect face

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -1,27 +1,33 @@
 import cv2
 import numpy as np
+import dlib
 
-
+detector = dlib.get_frontal_face_detector()
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-winStride = 10
-scale = 10
+winStride = (8,8)
+scale = 1.2
 
 
 def getRect(frame):
     rects, weights = hog.detectMultiScale(
-        frame, winStride, scale, useMeanshiftGrouping=False)
-    print(rects)
+        frame, winStride=winStride, scale=scale, useMeanshiftGrouping=False)
+    print(weights)
 
-
+def getFaces(frame):
+    face_rects = detector(frame, 0)
+    for i, d in enumerate(face_rects):
+        frame = cv2.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), (0,0,255), 2)
+    return frame
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         raise RuntimeError("should not happen")
     while True:
         ret, frame = cap.read()
-        getRect(frame)
+        # getRect(frame)
+        frame = getFaces(frame)
         cv2.imshow("sdf", frame)
         cv2.waitKey(1)
     pass
